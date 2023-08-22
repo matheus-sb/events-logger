1d2a986b1feb6233475ee65a0b3566c1e5d44ad2
Matheus <matheus.sob@gmail.com>
2023-08-22 10:15:37 -0300

    Added crew manager route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProjectComponent } from './project/project.component';
+import { CrewManagerComponent } from './crew-manager/crew-manager.component';
 
 const routes: Routes = [
   { path: '', component: ProjectComponent },
   { path: 'projects', component: ProjectComponent },
-  { path: 'employees', component: ProjectComponent },
+  { path: 'crew-managers', component: CrewManagerComponent },
   { path: 'event-logger', component: ProjectComponent },
   { path: 'total-hours-per-project', component: ProjectComponent },
   { path: 'total-hours-per-crew', component: ProjectComponent },
